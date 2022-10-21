12c360b158da180f19b259502aedf03cc164a43d
David Rojo <davidrojo1997@gmail.com>
2022-10-22 00:37:22 +0200

    feat(core): add TestGroup

--- src/core/test-group.ts
+++ src/core/test-group.ts
@@ -1,27 +1,69 @@
 import { Assertion } from '../assertions/index';
+import { groupDescriptionSymbol, groupResultsSymbol } from './symbols';
 import { EndedTest } from '../types/test-result.type';
+import { MockTestGroup } from './index';
+import { Result } from './test.result';
+import { ITestGroup } from './interfaces/index';
 
-export class TestGroup {
-  private _results: EndedTest[] = [];
+export class TestGroup implements ITestGroup {
+  protected _results: EndedTest[] = [];
 
-  get description(): string {
-    return this._description;
-  }
-
-  get results(): EndedTest[] {
-    return this._results;
-  }
+  [groupDescriptionSymbol] = this._description;
+  [groupResultsSymbol] = this._results;
 
   constructor(private readonly _description: string) {}
 
-  do<T>(
-    testCaseDescription: string,
-    assertion: T extends Assertion<T> ? T : never,
-  ): this {
+  /**
+   *
+   * Executes an assertion
+   *
+   * @param description - Description of the test
+   * @param assertion - Assertion to execute
+   * @returns TestGroup
+   */
+  do<T>(description: string, assertion: T extends Assertion<T> ? T : never): this {
     this._results.push({
-      description: testCaseDescription,
+      description,
       result: assertion.assert(),
     });
     return this;
+  }
+
+  /**
+   *
+   * Skips the assertion result
+   *
+   * @param description -  Description of the test
+   * @param assertion - Assertion to skip
+   * @returns TestGroup
+   */
+  skip(description: string, assertion: any): this {
+    const handler = (description: string) => {
+      this._results.push({
+        description,
+        result: Result.skip(),
+      });
+    };
+    handler(description);
+    return this;
+  }
+
+  /**
+   *
+   * Skips this assertion and the following ones in the call chain
+   *
+   * @param description - Description of the test
+   * @param assertion - Assertion to skip
+   * @returns TestGroup
+   */
+  skipChain(description: string, assertion: any): this {
+    const handler = (description: string) => {
+      this._results.push({
+        description,
+        result: Result.skip(),
+      });
+    };
+    handler(description);
+    return new MockTestGroup(handler) as unknown as this;
   }
 }
